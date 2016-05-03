d3425f345f0d8046c767e7d2a90083969d0d3b5f
José María Viceira <jmviceirapp@gmail.com>
2016-05-03 23:18:37 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,20 @@
 var models = require('../models');
 
+
+// Autoload el quiz asociadp a :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.findById(quizId)
+	.then(function(quiz) {
+		if (quiz) {
+			req.quiz = quiz;
+			next();
+		} else {
+			next(new Error('No existe quizId = ' + quizId));
+		}
+	}).catch(function(error) {next(error); });
+};
+
+
 // GET /quizzes
 exports.index = function(req, res, next) {
 	models.Quiz.findAll()
@@ -15,7 +30,7 @@ exports.show = function (req, res, next) {
 	.then(function(quiz){
 		if (quiz) {
 			var answer = req.query.answer || '';
-			res.render('quizzes/show', {quiz: quiz, answer: answer});
+			res.render('quizzes/show', {quiz: req.quiz, answer: answer});
 		} else { throw new Error ('No existe ese quiz en la BBDD.'); }
 	})
 	.catch(function(error) { next(error);});
@@ -27,8 +42,8 @@ exports.check = function (req, res, next) {
 	.then(function(quiz) {
 		if (quiz) {
 				var answer = req.query.answer || "";
-				var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
-				res.render('quizzes/result', { quiz: quiz, result: result, answer: answer });
+				var result = answer === req.quiz.answer ? 'Correcta' : 'Incorrecta';
+				res.render('quizzes/result', { quiz: req.quiz, result: result, answer: answer });
 		} else{	throw new Error('No existe ese quiz en la BBDD.');}
 	})
 	.catch(function(error) { next(error);});
