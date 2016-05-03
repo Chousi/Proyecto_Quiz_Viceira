8ba75c416d0d0176e9d84353068278c999d6dcbc
José María Viceira <jmviceirapp@gmail.com>
2016-05-03 14:45:00 +0200

    Lista de preguntas

--- models/index.js
+++ models/index.js
@@ -1,53 +1,54 @@
+
 var path = require('path');
 
-// Cargar Modelo ORM
+// Cargar ORM
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite:
- //  DATABASE_URL = sqlite:///
- //  DATABASE_STORAGE = quiz.sqlite
+//    DATABASE_URL = sqlite:///
+//    DATABASE_STORAGE = quiz.sqlite
 // Usar BBDD Postgres:
- // DATABASE_URL = postgres://upostgres://apsxqkqsttkkjm:vG38vD5q_uL4N-P8j3I9B-5TXF@ec2-54-243-195-46.compute-1.amazonaws.com:5432/dee2j847veip7l
+//    DATABASE_URL = postgres://user:passwd@host:port/database
 
 var url, storage;
 
 if (!process.env.DATABASE_URL) {
-     url = "sqlite:///";
-     storage = "quiz.sqlite";
+    url = "sqlite:///";
+    storage = "quiz.sqlite";
 } else {
-     url = process.env.DATABASE_URL;
-     storage = process.env.DATABASE_STORAGE || "";
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
 }
 
-var sequelize = new Sequelize(url,
+var sequelize = new Sequelize(url, 
                               { storage: storage,
-                                omitNull: true
+                                omitNull: true 
                               });
 
+// Importar la definicion de la tabla Quiz de quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
-// Importar la definición de la tabla Quiz de quiz.js
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize
-.sync()
-.then(function() { // sync() crea la tabla Quiz
-   return
-     Quiz
-     .count()
-     .then(function (c) {
-     	if (c === 0) { // la tabla se inicializa si está vacía
-     		return
-     		 Quiz
-     		 .create({ question: 'Capital de Italia', answer: 'Roma' })
-     		 .then(function(){
-     		 	console.log('Base de datos inicializada con datos');
-     		 });
-     	}
-     });
-}).catch(function(error) {
-	console.log("Error Sincronizando las tablas de la BBDD:", error);
-	process.exit(1);
-});
+sequelize.sync()
+    .then(function() {
+        // Ya se han creado las tablas necesarias.
+        return Quiz.count()
+                .then(function (c) {
+                    if (c === 0) {   // la tabla se inicializa solo si está vacía
+                        return Quiz.bulkCreate([ {question: 'Capital de Italia',   answer: 'Roma'},
+                                                 {question: 'Capital de Portugal', answer: 'Lisboa'}
+                                              ])
+                                   .then(function() {
+                                        console.log('Base de datos inicializada con datos');
+                                    });
+                    }
+                });
+    })
+    .catch(function(error) {
+        console.log("Error Sincronizando las tablas de la BBDD:", error);
+        process.exit(1);
+    });
 
-exports.Quiz = Quiz; // exportar definición de la tabla Quiz
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,18 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index');
-});
-
 router.get('/author', function (req, res, next) {
 	res.render('author');
 })
 
-router.get('/question', quizController.question);
-router.get('/check', quizController.check);
+/* GET home page. */
+router.get('/', function (req, res, next) {
+	res.render('index');
+});
 
+// Definición de rutas de /quizzes
+router.get('/quizzes',						quizController.index);
+router.get('/quizzes/:quizId(\\d+)',		quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check',	quizController.check);
 
 module.exports = router;
